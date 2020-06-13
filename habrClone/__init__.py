0b3ad01c4a256dac5ddfe11c3f2fdb495f10c0b5
Architect <greatraksin@icloud.com>
2020-06-13 12:24:06 +0300

    Forms part one. Adding login form: configuration file, form model, template and validating

--- habrClone/__init__.py
+++ habrClone/__init__.py
@@ -1,5 +1,7 @@
 from flask import Flask  # из библиотеки flask импортируем КЛАСС Flask
+from config import Config
 
 App = Flask(__name__)
+App.config.from_object(Config)
 
 from habrClone import routes  # импортировали маршруты
--- habrClone/routes.py
+++ habrClone/routes.py
@@ -1,8 +1,18 @@
 from habrClone import App
-from flask import render_template
+from flask import render_template, flash, redirect, url_for
+from habrClone.forms import LoginForm
 
 
 @App.route('/')
 @App.route('/index')
 def index():
     return render_template('index.html')
+
+
+@App.route('/sign_in', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash(f'Пользователь {form.username.data} вошел. Поле "запомнить" {form.remember_me.data}.')
+        return redirect(url_for('index'))
+    return render_template('login.html', form=form)
