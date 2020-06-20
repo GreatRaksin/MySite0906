ff1afea48ceafd27242542bac6a61c87d3b8ee9a
Architect <greatraksin@icloud.com>
2020-06-21 01:52:11 +0300

    Adding registration

--- habrClone/forms.py
+++ habrClone/forms.py
@@ -1,6 +1,8 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, BooleanField, SubmitField
-from wtforms.validators import DataRequired
+from wtforms.validators import DataRequired, ValidationError, EqualTo
+
+from habrClone.models import User
 
 
 class LoginForm(FlaskForm):
@@ -8,3 +10,21 @@ class LoginForm(FlaskForm):
     password = PasswordField('Пароль', validators=[DataRequired()])
     remember_me = BooleanField('Запомнить меня')
     submit = SubmitField('Войти')
+
+
+class RegistrationForm(FlaskForm):
+    username = StringField(validators=[DataRequired()])
+    email = StringField(validators=[DataRequired()])
+    password = StringField(validators=[DataRequired()])
+    password2 = StringField(validators=[DataRequired(), EqualTo('password')])
+    submit = SubmitField('Регистрация')
+
+    def validate_username(self, username):
+        user = User.query.filter_by(username=username.data).first()
+        if user is not None:
+            raise ValidationError('Используйте другое имя!')
+
+    def validate_email(self, email):
+        user = User.query.filter_by(email=email.data).first()
+        if user is not None:
+            raise ValidationError('Используйте другой Email!')
--- habrClone/routes.py
+++ habrClone/routes.py
@@ -1,8 +1,8 @@
 from flask import render_template, flash, redirect, url_for
 from flask_login import current_user, login_user, logout_user
 
-from habrClone import App
-from habrClone.forms import LoginForm
+from habrClone import App, db
+from habrClone.forms import LoginForm, RegistrationForm
 from habrClone.models import User
 
 
@@ -40,3 +40,22 @@ def login():
 def logout():
     logout_user()
     return redirect(url_for('index'))
+
+
+@App.route('/sign_up', methods=['GET', 'POST'])
+def register():
+    '''проверка текущего сеанса пользователя (вошел или нет)'''
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    form = RegistrationForm()
+
+    if form.validate_on_submit():
+        user = User(username=form.username.data, email=form.email.data)
+        user.set_password(form.password.data)
+        db.session.add(user)
+        db.session.commit()
+
+        flash('Вы успешно зарегистрировались!')
+
+        return redirect(url_for('login'))
+    return render_template('register.html', form=form, title='Регистрация')
