bcb39ebf266e2ff71873e0df645e7c7378916c07
Architect <greatraksin@icloud.com>
2020-06-21 00:56:28 +0300

    Adding login system

--- habrClone/routes.py
+++ habrClone/routes.py
@@ -1,6 +1,9 @@
-from habrClone import App
 from flask import render_template, flash, redirect, url_for
+from flask_login import current_user, login_user, logout_user
+
+from habrClone import App
 from habrClone.forms import LoginForm
+from habrClone.models import User
 
 
 @App.route('/')
@@ -11,8 +14,29 @@ def index():
 
 @App.route('/sign_in', methods=['GET', 'POST'])
 def login():
+    '''проверка текущего сеанса пользователя (вошел или нет)'''
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
     form = LoginForm()
+
+    '''когда форма отправляется'''
     if form.validate_on_submit():
-        flash(f'Пользователь {form.username.data} вошел. Поле "запомнить" {form.remember_me.data}.')
+        '''находим пользователя в базе'''
+        user = User.query.filter_by(username=form.username.data).first()
+        '''если мы его там не нашли (None) или пароль невеный'''
+        if user is None or not user.check_password(form.password.data):
+            '''даем ему сообщение и возвращаем на страницу с логином'''
+            flash('Неправильное имя пользователя и/или пароль!')
+            return redirect(url_for('login'))
+
+        '''если все ок, выполняем вход (login_user) и перенаправляем на главную'''
+        login_user(user, remember=form.remember_me.data)
+
         return redirect(url_for('index'))
     return render_template('login.html', form=form)
+
+
+@App.route('/sign_out')
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
