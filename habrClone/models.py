cf7fe5a1a04d78c547fb2b01694a00c646a563cd
Architect <greatraksin@icloud.com>
2020-06-21 00:32:48 +0300

    Adding database and configuration

--- habrClone/models.py
+++ habrClone/models.py
@@ -1,29 +1,25 @@
 from habrClone import db
 from datetime import datetime
-from werkzeug.security import generate_password_hash, check_password_hash
 
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(20), unique=True, nullable=False, index=True)
-    email = db.Column(db.String(120), unique=True, nullable=False)
-    password = db.Column(db.String(20), nullable=False)
-    avatar = db.Column(db.String(20), nullable=False, default='default.jpg')
-
+    username = db.Column(db.String(20), unique=True, index=True)
+    email = db.Column(db.String(120), unique=True)
+    password = db.Column(db.String(20))
+    avatar = db.Column(db.String(20), default='default.jpg')
     news = db.relationship('New', backref='author', lazy=True)
 
-    def set_password(self, password):
-        self.password_hash = generate_password_hash(password)
-
-    def check_password(self, password):
-        return check_password_hash(self.password_hash, password)
+    def __repr__(self):
+        return f'Пользователь: {self.username}, email {self.email}'
 
 
 class New(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    article = db.Column(db.String(100), nullable=False)
-    publish_date = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
-    body = db.Column(db.Text, nullable=False)
+    article = db.Column(db.String(100))
+    publish_date = db.Column(db.DateTime, default=datetime.utcnow)
+    body = db.Column(db.Text)
 
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    
 
